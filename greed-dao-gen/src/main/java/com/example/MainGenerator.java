ee8ac0e48377775f7d9ac97d52bedf6100059e44
Kanval Kalra <kanval87@gmail.com>
2017-02-06 10:59:38 +0530

    1. added greenDao for db operations

---
+++ greed-dao-gen/src/main/java/com/example/MainGenerator.java
@@ -0,0 +1,41 @@
+package com.example;
+
+import de.greenrobot.daogenerator.DaoGenerator;
+import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Schema;
+
+public class MainGenerator {
+
+    private static final String PROJECT_DIR = System.getProperty("user.dir");
+
+    public static void main(String[] args) {
+        Schema schema = new Schema(1, "com.kanvalkalra.colormemory.db");
+        schema.enableKeepSectionsByDefault();
+
+        addTables(schema);
+
+        try {
+            /* Use forward slashes if you're on macOS or Unix, i.e. "/app/src/main/java"  */
+            new DaoGenerator().generateAll(schema, PROJECT_DIR + "/app/src/main/java");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void addTables(final Schema schema) {
+        Entity highScoreUser = addHighScoreUser(schema);
+    }
+
+    private static Entity addHighScoreUser(Schema schema) {
+        Entity highScoreUser = schema.addEntity("HighScoreUser");
+        highScoreUser.addIdProperty().primaryKey().autoincrement();
+        highScoreUser.addStringProperty("name").notNull();
+        highScoreUser.addShortProperty("score").notNull();
+
+        return highScoreUser;
+    }
+
+
+}
+
+
